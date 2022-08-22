ec0b1e6a05403eb77ec4d45190d970922ec97acd
Harsh Saxena <harsh.saxena@tigeranalytics.com>
2022-08-22 13:26:57 +0530

    Problem analysed and solved using jupyter notebook

--- docs/docs/source/conf.py
+++ docs/docs/source/conf.py
@@ -14,7 +14,7 @@ release = "0.3"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.coverage", "sphinx.ext.napoleon"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -23,5 +23,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
