82ec6397f041f1472cd4193ef7648f236f774ce5
Harsh Saxena <harsh.saxena@tigeranalytics.com>
2022-08-25 15:08:48 +0530

    Update test_installation.py

--- tests/functional_tests/test_installation.py
+++ tests/functional_tests/test_installation.py
@@ -5,8 +5,8 @@ class Test(unittest.TestCase):
 
     def test_package(self):
         self.predict=Predict()
-        res=my_package.__version__
-        self.assertEqual(res, '0.1')
+        res=my_package.__package__
+        self.assertEqual(res, 'my_package')
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
